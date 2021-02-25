8d70c68944b878a5b154c5d16deda49ce934c979
Tolga Secme <tolgasecme@icloud.com>
2021-02-24 22:32:15 -0700

    update

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const askQuestion = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            message: 'What is your preffered location?',
-            name: 'city',
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      message: 'Where are you from?',
+      name: 'city',
 
-        },
-        {
-            type: 'input',
-            message: 'What is your short Bio?',
-            name: 'Bio',
-        },
-        {
-            type: 'input',
-            message: 'What is your Linkedin Url?',
-            name: 'Linkedin',
-        },
-        {
-            type: 'input',
-            message: 'What is your Github Url?',
-            name: 'Github',
-        },
-    ])
+    },
+    {
+      type: 'input',
+      message: 'What is your short Bio?',
+      name: 'Bio',
+    },
+    {
+      type: 'input',
+      message: 'What is your Linkedin Url?',
+      name: 'Linkedin',
+    },
+    {
+      type: 'input',
+      message: 'What is your Github Username?',
+      name: 'Github',
+    },
+  ])
 }
 const makeHtmlPage = (data) =>
-    `<!DOCTYPE html>
+  `<!DOCTYPE html>
     <html lang="en">
     
     <head>
@@ -47,12 +47,13 @@ const makeHtmlPage = (data) =>
     <body>
       <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-4">Hi! My name is ${data.name}</h1>
-          <p class="lead">I am from ${data.city}.</p>
-          <p class="lead">Short Bio About Me: ${data.Bio}</p>
+          <h1 class="display-4">Hi! My name is <strong>${data.name}</strong></h1>
+          <p class="lead">I am from <strong>${data.city}</strong>.</p>
+          <h2 class="display-4">Short Bio About Me:</h2>
+          <p class="lead">${data.Bio}</p>
           <h3><span class="badge badge-secondary">Contact Me</span></h3>
           <ul class="list-group">
-            <li class="list-group-item">LinkedIn: ${data.Linkedin}</li>
+            <li class="list-group-item">My Linkedin: ${data.Linkedin}</li>
             <li class="list-group-item">My GitHub username is ${data.Github}</li>
           </ul>
         </div>
@@ -62,9 +63,9 @@ const makeHtmlPage = (data) =>
     </html>`
 
 const init = () => {
-    askQuestion()
-        .then((answers) => writeFileAsync('index.html', makeHtmlPage(answers)))
-        .then(() => console.log('Succesfuly wrote!'))
-        .catch((err) => console.error(err));
+  askQuestion()
+    .then((answers) => writeFileAsync('index.html', makeHtmlPage(answers)))
+    .then(() => console.log('Succesfuly wrote!'))
+    .catch((err) => console.error(err));
 };
 init();
